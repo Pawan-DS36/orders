544191e8979495212a1947cb7d72bc35d71b8096
Pawan-DS36 <mahapurepawan@gmail.com>
2025-05-29 11:41:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ st.subheader("View and Filter Orders from CSV")
 data = pd.read_csv(orders.csv)
     
     # Show Full Data
-    st.write("### All Orders")
-    st.dataframe(data)
+st.write("### All Orders")
+st.dataframe(data)
 
     # Sidebar - Filter by City
-    cities = st.sidebar.multiselect("Filter by City", data['City'].unique())
-    if cities:
-        data = data[data['City'].isin(cities)]
-        st.write("### Filtered Orders")
-        st.dataframe(data)
+cities = st.sidebar.multiselect("Filter by City", data['City'].unique())
+if cities:
+    data = data[data['City'].isin(cities)]
+    st.write("### Filtered Orders")
+    st.dataframe(data)
 
     # Summary
-    total_orders = data['OrderID'].nunique()
-    total_revenue = (data['Price'] * data['Quantity']).sum()
+total_orders = data['OrderID'].nunique()
+total_revenue = (data['Price'] * data['Quantity']).sum()
 
-    st.write("### Summary")
-    st.write("Total Orders:", total_orders)
-    st.write("Total Revenue:", total_revenue)
+st.write("### Summary")
+st.write("Total Orders:", total_orders)
+st.write("Total Revenue:", total_revenue)
